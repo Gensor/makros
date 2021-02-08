0c9a41a7ca45c9445c853eebd85be49e6313f501
Gensor <Gensor@users.noreply.github.com>
2021-02-08 11:35:21 +0100

    Initial commit

---
+++ src/main/java/com/gensor/makros/MakrosApplication.java
@@ -0,0 +1,13 @@
+package com.gensor.makros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gensor/makros/MakrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gensor.makros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
